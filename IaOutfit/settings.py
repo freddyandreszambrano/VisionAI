566229afbfe7dc8769c78998a8b02e06eabcef3f
Freddy Zambrano <freddyfazq0614@gmail.com>
2024-05-04 20:27:33 -0500

    :twisted_rightwards_arrows: Cambios de nombres y eliminación de carpetas

--- IaOutfit/settings.py
+++ IaOutfit/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'IaOutfit'
+    'IaOutfit',
+    'Apps.Main',
+    'Apps.OutfitGeneration',
+    'Apps.OutfitSaving',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/' # url de archivos estaticos
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)# carpeta fisica de archivos estaticos
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- IaOutfit/urls.py
+++ IaOutfit/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Outfit.urls')),
-]
+    path('prueba/', include('Outfit.urls')),
+  
+    path('Main/',include('Apps.Main.urls',namespace="Main") ),
+  
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
