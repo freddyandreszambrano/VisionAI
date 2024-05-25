ba4c71dbc4abff909f74efb5bbcaba4534ec8576
Mikel <k.Prueba.31.01@gmail.com.com>
2024-05-24 23:34:46 -0500

    crispy

--- IaOutfit/settings.py
+++ IaOutfit/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +47,9 @@ INSTALLED_APPS = [
     'Apps.Accounts',
     'Apps.Wardrobe',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
