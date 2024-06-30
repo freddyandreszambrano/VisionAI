c0ad564de6f38f085e8a2bf5b790524f40790e6d
Mikel <k.Prueba.31.01@gmail.com.com>
2024-06-30 16:05:01 -0500

    detalle de prenda

--- Apps/Main/views.py
+++ Apps/Main/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from Apps.Wardrobe.models import Clothes
 
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from Apps.Wardrobe.models import Clothes
 
@@ -10,9 +10,15 @@ class RopaListView(ListView):
     template_name = 'Home/main.html'
     context_object_name = 'prendas'
 
+def prenda_detail(request, prenda_id):
+    prenda = get_object_or_404(Clothes, id=prenda_id)
+    # print(prenda.type)
+    return render(request, 'Item_details/details.html', {'prenda': prenda})
 
 def delete_prenda(request, prenda_id):
     prenda = get_object_or_404(Clothes, id=prenda_id)
     prenda.delete()
     return redirect(reverse('Main:Main_list'))
+
+
 
