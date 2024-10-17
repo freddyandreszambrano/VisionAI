1cacbe6e3cfa0432a02a859d8473dc105c9642d5
Freddy Zambrano <freddyfazq0614@gmail.com>
2024-10-17 06:54:19 -0500

    first commit

--- Apps/OutfitGeneration/urls.py
+++ Apps/OutfitGeneration/urls.py
@@ -1,15 +1,8 @@
 
 from django.urls import path,include
-from Apps.OutfitGeneration.views import OutfitGenerationView, PreseleccionView,SeleccionPrendasOutfitGeneratorView,Eleccion_filtrosView,ProcessSelectionView,ShowSelectionView,GuardarOutfitView,FiltrosOutfitGenerationView
+from Apps.OutfitGeneration.views import PreseleccionView
 
 app_name = 'OutfitGeneration'
 urlpatterns = [
-    path('outfit_generation/', OutfitGenerationView.as_view(), name='outfit_generation'),
     path('outfit_preseleccion/', PreseleccionView.as_view(), name='outfit_preseleccion'),
-    path('seleccion_prendas_outfit_generacion/', SeleccionPrendasOutfitGeneratorView.as_view(), name='seleccion_prendas_outfit_generacion'),
-    path('Presentar_filtros/', Eleccion_filtrosView.as_view(), name='Presentar_filtros'),
-    path('process_selection/', ProcessSelectionView.as_view(), name='process_selection'),
-    path('show_selection/', ShowSelectionView.as_view(), name='show_selection'),
-    path('Outfit_saving/', GuardarOutfitView.as_view(), name='outfit_saving'),
-    path('Filtros_outfit_generation/', FiltrosOutfitGenerationView.as_view(), name='filtros_outfit_generation'),
 ]
